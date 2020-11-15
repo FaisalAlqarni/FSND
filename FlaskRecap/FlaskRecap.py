cc8d11b1fa6ae234d7d26ce1c23d626eba0cdfc4
Faisal Alqarni <Falqarni@mrsool.co>
2020-11-15 04:56:50 +0300

    Update FlaskRecap.py

--- FlaskRecap/FlaskRecap.py
+++ FlaskRecap/FlaskRecap.py
@@ -3,26 +3,28 @@ from flask import Flask, request, jsonify, abort
 app = Flask(__name__)
 
 greetings = {
-            'en': 'hello', 
-            'es': 'Hola', 
-            'ar': 'مرحبا',
-            'ru': 'Привет',
-            'fi': 'Hei',
-            'he': 'שלום',
-            'ja': 'こんにちは'
-            }
+    'en': 'hello',
+    'es': 'Hola',
+    'ar': 'مرحبا',
+    'ru': 'Привет',
+    'fi': 'Hei',
+    'he': 'שלום',
+    'ja': 'こんにちは'
+}
+
 
 @app.route('/greeting', methods=['GET'])
 def greeting_all():
     return jsonify({'greetings': greetings})
+
 
 @app.route('/greeting/<lang>', methods=['GET'])
 def greeting_one(lang):
     print(lang)
     if(lang not in greetings):
         abort(404)
-    return jsonify({'greeting': greetings[lang
-    ]})
+    return jsonify({'greeting': greetings[lang]})
+
 
 @app.route('/greeting', methods=['POST'])
 def greeting_add():
@@ -30,4 +32,4 @@ def greeting_add():
     if('lang' not in info or 'greeting' not in info):
         abort(422)
     greetings[info['lang']] = info['greeting']
-    return jsonify({'greetings':greetings})
+    return jsonify({'greetings': greetings})

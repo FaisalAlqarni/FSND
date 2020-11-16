1552bdfd89b37c70d395c8797d0d9dfe86d62c06
Faisal Alqarni <Falqarni@mrsool.co>
2020-11-16 11:48:53 +0300

    apply pep8 standards

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -9,18 +9,20 @@ AUTH0_DOMAIN = 'faisal-alqarni-misk.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'coffee'
 
-## AuthError Exception
+# AuthError Exception
 '''
 AuthError Exception
 A standardized way to communicate auth failure modes
 '''
+
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
 
 
-## Auth Header
+# Auth Header
 
 '''
 @TODOx implement get_token_auth_header() method
@@ -31,20 +33,22 @@ class AuthError(Exception):
     return the token part of the header
 '''
 # inspiered from Udacity classroom lessons
+
+
 def get_token_auth_header():
     # get the header from the request
     auth_header = request.headers.get('Authorization', None)
-    
+
     # raise an AuthError if no header is present
     if not auth_header:
         raise AuthError({
             'code': 'authorization_header_missing',
             'description': 'Authorization header is expected.'
         }, 401)
-        
+
     # split bearer and the token
     splitted_auth = auth_header.split()
-    
+
     # raise an AuthError if the header is malformed
     if splitted_auth[0].lower() != 'bearer':
         raise AuthError({
@@ -63,10 +67,12 @@ def get_token_auth_header():
             'code': 'invalid_header',
             'description': 'Authorization header must be bearer token.'
         }, 401)
-        
+
     # return the token part of the header
     token = splitted_auth[1]
     return token
+
+
 '''
 @TODOX implement check_permissions(permission, payload) method
     @INPUTS
@@ -78,23 +84,17 @@ def get_token_auth_header():
     it should raise an AuthError if the requested permission string is not in the payload permissions array
     return true otherwise
 '''
+
+
 def check_permissions(permission, payload):
-    # raise an AuthError if permissions are not included in the payload
-    if permission not in payload:
-        raise AuthError({
-            'code': 'invalid_claims',
-            'description': 'permission key not included',
-        }, 400)  
-        
-    # raise an AuthError if the requested permission string is not in the payload permissions array
     if permission not in payload['permissions']:
         raise AuthError({
             'code': 'unauthorized',
             'description': 'permission key not found',
         }, 401)
-        
-    # return true otherwise
+
     return True
+
 
 '''
 @TODOX implement verify_decode_jwt(token) method
@@ -110,6 +110,8 @@ def check_permissions(permission, payload):
     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
 # inspiered from Udacity classroom lessons
+
+
 def verify_decode_jwt(token):
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
@@ -159,9 +161,9 @@ def verify_decode_jwt(token):
                 'description': 'Unable to parse authentication token.'
             }, 400)
     raise AuthError({
-                'code': 'invalid_header',
+        'code': 'invalid_header',
                 'description': 'Unable to find the appropriate key.'
-            }, 400)
+    }, 400)
 
 
 '''
@@ -174,6 +176,8 @@ def verify_decode_jwt(token):
     it should use the check_permissions method validate claims and check the requested permission
     return the decorator which passes the decoded payload to the decorated method
 '''
+
+
 def requires_auth(permission):
     def requires_auth_decorator(f):
         @wraps(f)
